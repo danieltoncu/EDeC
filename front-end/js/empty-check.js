019e6181cd3a77f9d1a687e4ee966a6d700fa2c3
danieltoncu <daniel.toncu@info.uaic.ro>
2016-06-05 08:41:50 +0300

    Am adaugat Live Search Bar pentru Pagina MyAccount si am creat Pagina Statistics.

--- front-end/js/empty-check.js
+++ front-end/js/empty-check.js
@@ -1,6 +1,23 @@
 function searchValidate() {
+
 	var searchInput = document.getElementById("search-0D1").value;
+
 	if (searchInput == null || searchInput == "") {
+		return false;
+	}
+}
+
+function ingredientSearchValidate() {
+
+	var ingredientSearchInput = document.getElementById("search-0D2").value;
+	var ingredientLike = document.getElementById("radio-0DL").checked;
+	var ingredientDislike = document.getElementById("radio-0DD").checked;
+
+	if (ingredientSearchInput == null || ingredientSearchInput == "") {
+		return false;
+	}
+
+	if (!(ingredientLike || ingredientDislike)) {
 		return false;
 	}
 }
